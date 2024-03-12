c38d9900b00bd729a807e7d27ace99f8e85f5dce
UsmeGallego101206 <127226634+UsmeGallego101206@users.noreply.github.com>
2024-03-12 01:01:49 -0500

    a

--- index.js
+++ index.js
@@ -14,14 +14,40 @@ if(aproved == false) {
     console.log('Its approved');
 }*/
 
-function getClietIP(){
+/*function getClietIP(){
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position){    
             var ip = position.coords.latitude + "," + position.coords.longitude;    
-            alert(`Your IP adress is: ${ip}`);  
+            alert(`Your IP adress is: ${ip}`);
+            
         });
     }  else{
         alert("Geolocation is not supported by your browser and this application does not work properly without it.");
 }
 }
 getClietIP();
+*/
+const mapDiv = document.getElementById("map");
+let map;
+function initMap() {
+navigator.geolocation.getCurrentPosition(function(position){    
+    var ip = position.coords.latitude;
+    var ip2 = position.coords.longitude;
+    console.log(`<p> Your latitude is : ${ip}</p>`);
+    console.log(`<p> Your longitude is : ${ip2}</p>`);
+    map = new google.maps.Map(mapDiv, {
+    center: {lat: ip, lng: ip2},
+    zoom: 8,
+  });
+  marker = new google.maps.Marker({
+      position:{
+          lat: ip,
+          lng: ip2
+      },
+      map: map,
+      title:"You are here!"
+  })    
+    });
+  
+}
+initMap();
